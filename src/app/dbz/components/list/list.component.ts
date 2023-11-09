9e42113b9de7e2d644014e9815a4856125bfea4d
gabriela.cordova <gabriela.cordovacr@scc.com.pe>
2023-11-08 22:00:49 -0500

    primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  @Input()
+
+  public characterList: Character[]=[{
+    name:'Trunks',
+    power: 10
+  }]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    // onDelete = Index value : number
+    @Output()
+    public onDelete: EventEmitter<string> = new EventEmitter();
+
+
+    onDeleteCharacter( id?: string ):void {
+
+      if ( !id ) return;
+      this.onDelete.emit( id );
+    }
+
+}
